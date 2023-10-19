1258cd08e14bf4c1d42758bdcf82a7ca47c2407d
Harshit Mehndiratta <36616432+harshitm1234@users.noreply.github.com>
2023-10-19 15:02:15 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,9 @@ function Index() {
       <Box sx={{height: `calc(100vh - 100px)` }}>
         <Box sx={{ height: '100%' }}>
           <img src={'/dashboard.jpg'} width="100%" height="100%" />
+  <a className="App-link" href="http://localhost:3001/apply-loan/emandate/payment" rel="noopener noreferrer">
+          Learn React
+        </a>
         </Box>
       </Box>
     </AppLayout>
